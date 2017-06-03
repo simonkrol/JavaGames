c94301715d223786ea64f79245a3ce60edca3047
simonkrol <simonkrol11@gmail.com>
2017-06-02 23:36:02 -0400

    Add inventory class

---
+++ src/character_work_7/Inventory.java
@@ -0,0 +1,13 @@
+package character_work_7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+public class Inventory {
+	int capacity;
+	JFrame inv;//Maybe not a JFrame?
+	List<Item> items=new ArrayList<Item>();
+
+}
---
+++ src/character_work_7/Item.java
@@ -0,0 +1,10 @@
+package character_work_7;
+
+public class Item {
+	
+	String name;
+	int value;
+	String rarity;
+	String description;
+	String use;
+}
